7561c1e25289d748147126646bd4b651580cb82f
Aditya Wahyu Pratama <adityawahyu131@gmail.com>
2022-09-05 09:10:26 +0700

    Initial Commit

---
+++ Adaptive Design/Adaptive_DesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  Adaptive_DesignApp.swift
+//  Adaptive Design
+//
+//  Created by Adit on 05/09/22.
+//
+
+import SwiftUI
+
+@main
+struct Adaptive_DesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
